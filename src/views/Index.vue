3a47d13ac47dde2e3f6eeefb46ed12005189d1b1
Jefry Dewangga <jefrydco@gmail.com>
2021-03-31 11:23:50 +0700

    feat: add link to each page

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,9 +1,33 @@
-<script>
-import { useRouter } from 'vue-router'
+<template>
+  <ol class="m-10">
+    <li>
+      <router-link to="/z-index">/z-index</router-link>
+    </li>
+    <!-- <li>
+      <router-link to="/z-index-visualize">/z-index-visualize</router-link>
+    </li> -->
+    <!-- <li>
+      <router-link to="/keyframe-animation">/keyframe-animation</router-link>
+    </li> -->
+    <li>
+      <router-link to="/transition">/transition</router-link>
+    </li>
+    <li>
+      <router-link to="/z-index-keyframe">/z-index-keyframe</router-link>
+    </li>
+    <li>
+      <router-link to="/state-machine">/state-machine</router-link>
+    </li>
+  </ol>
+</template>
 
-export default {
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
   setup() {
-    useRouter().replace('/z-index')
+    
   },
-}
+})
 </script>
+
